da35e02ea4ef3ddcb58820c1b7e613f3d9e582f8
Renato Deck-Dev <deeckinve@proton.me>
2023-11-30 18:34:50 -0300

    Interface criada e lógica de mudança de estados da interface implementada

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,90 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  int _people = 0;
+  String _infoText = 'Pode Entrar!';
+
+  void _changePeople(int delta) {
+    setState(() {
+      _people += delta;
+
+      if (_people < 0) {
+        _infoText = 'Mundo Invertido?!';
+      } else if (_people <= 10) {
+        _infoText = 'Pode Entrar!';
+      } else {
+        _infoText = 'Lotado';
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Contador de Pessoas',
-      home: Container(
-        color: Colors.white,
+      home: Stack(
+        children: [
+          Image.asset(
+            'assets/images/img.png',
+            fit: BoxFit.cover,
+            height: 1000.0,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Pessoas:$_people',
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: TextButton(
+                      onPressed: () {
+                        _changePeople(1);
+                      },
+                      child: const Text(
+                        '+1',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 40,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: TextButton(
+                      onPressed: () {
+                        _changePeople(-1);
+                      },
+                      child: const Text(
+                        '-1',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 40,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Text(
+                _infoText,
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30,
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
